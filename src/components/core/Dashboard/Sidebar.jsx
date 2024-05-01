174872d6559599ca9cf1d9235105d28bccca8113
kash2104 <kparikh1104@gmail.com>
2024-05-01 18:55:17 +0530

    Logout modal completed. The sidebar(navbar) of dashboard completed.

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { sidebarLinks } from '../../../data/dashboard-links'
 import { logout } from '../../../services/operations/authAPI'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import SidebarLink from './SidebarLink'
-import { VscSettingsGear } from 'react-icons/vsc'
+import { VscSettingsGear, VscSignOut } from 'react-icons/vsc'
+import { useNavigate } from 'react-router-dom'
+import ConfirmationModal from '../../common/ConfirmationModal'
 
 
 const Sidebar = () => {
     const{user, loading:profileLoading} = useSelector((state) => state.profile);
 
     const{loading:authLoading} = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    //in starting I don't know what should be shown to the user. Once I click that button, then only I will get to know.
+    const[confirmationModal, setConfirmationModal] = useState(null);
 
     if(profileLoading || authLoading){
         return(
@@ -43,10 +51,31 @@ const Sidebar = () => {
                 <SidebarLink link={{name:'Settings', path:'dashboard/settings'}} iconName='VscSettingsGear'/>
 
                 {/* logout -> onClick it shows a modal so have to make that modal */}
+
+                <button onClick={() => setConfirmationModal(
+                    {
+                    text1: 'Are You Sure?',
+                    text2:'You will be logged out of your account',
+                    btn1Text:'Logout',
+                    btn2Text:'Cancel',
+                    btn1Handler:() => dispatch(logout(navigate)),
+                    
+                    //onClicking the cancel button, the modal should hide so set the value to null.
+                    btn2Handler:() => setConfirmationModal(null),
+
+                }
+                )} className='px-8 py-2 text-sm font-medium text-richblack-300'>
+                    <div className='flex items-center gap-x-2'>
+                        <VscSignOut className='text-lg'/>
+                        <span>Logout</span>
+                    </div>
+                </button>
             </div>
 
         </div>
-
+        
+        {/* when the value is null, the modal will not be visible but onClicking the data of modal will be set and it can be seen. */}
+        {confirmationModal && <ConfirmationModal modalData={confirmationModal}/>}
     </div>
   )
 }
