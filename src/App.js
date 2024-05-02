b11dda89252e92eaf072220cbddfbe0f4d88d7ae
kash2104 <kparikh1104@gmail.com>
2024-05-02 13:36:24 +0530

    SettingsAPI added in operations.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MyProfile from "./components/core/Dashboard/MyProfile";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/core/Auth/PrivateRoute";
 import Error from "./pages/Error"
+import Settings from "./components/core/Dashboard/Settings/Settings";
 
 function App() {
   return (
@@ -86,7 +87,7 @@ function App() {
           
             <Route path="dashboard/my-profile" element={<MyProfile/>}/>
 
-            {/* <Route path="dashboard/settings" element={<Setting/>}/> */}
+            <Route path="dashboard/settings" element={<Settings/>}/>
 
           </Route>
 
---
+++ src/components/core/Dashboard/Settings/Settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div>
+        <h1 className='text-3xl mb-14 font-medium text-richblack-5'>
+            Edit Profile
+        </h1>
+
+        
+    </div>
+  )
+}
+
+export default Settings
