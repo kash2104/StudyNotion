c2a082f3e90584350d8be8a1078aa5520f2568cd
kash2104 <kparikh1104@gmail.com>
2024-05-17 17:04:53 +0530

    courseProgress controller written

--- Server/controllers/courseProgress.js
+++ Server/controllers/courseProgress.js
@@ -1,24 +1,25 @@
-const mongoose = require("mongoose");
-const SubSection = require("../models/SubSection");
 const CourseProgress = require("../models/CourseProgress");
+const SubSection = require("../models/SubSection");
 
 exports.updateCourseProgress = async (req, res) => {
+  //jab bhi course me enroll hue i.e. jaise hi payment successfull hui tab hume batana padega ki abhi uski progress = 0 hain
   const { courseId, subSectionId } = req.body;
 
   const userId = req.user.id;
 
   try {
-    //check if the subSection is valid
+    //check if the subsection is valid or not
     const subSection = await SubSection.findById(subSectionId);
 
     if (!subSection) {
       return res.status(404).json({
-        error: "Invalid subSection while updatingCourseProgress",
+        success: false,
+        error: "Invalid subSection",
       });
     }
 
-    //find the courseProgress document for the user and that particular course course
-    let courseProgress = await CourseProgress.find({
+    //is subSection ki entry pehle se exist karti hain ya nhi
+    let courseProgress = await CourseProgress.findOne({
       courseID: courseId,
       userId: userId,
     });
@@ -26,30 +27,35 @@ exports.updateCourseProgress = async (req, res) => {
     if (!courseProgress) {
       return res.status(404).json({
         success: false,
-        message: "Course Progress does not exist",
+        message: "Course progress does not exist",
       });
     } else {
-      //checking if the subSection is completed or not
+      //courseProgress is valid
+
+      //checking if video is already marked as complete or not
       if (courseProgress.completedVideos.includes(subSectionId)) {
         return res.status(400).json({
-          error: "subSection is already completed",
+          success: false,
+          error: "Subsection is already marked as complete",
         });
       }
+
+      //push the video into completedVideo
+      courseProgress.completedVideos.push(subSectionId);
+
+      //need to save this updated state
+      await courseProgress.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Course Progress updated",
+      });
     }
-
-    //adding the subSection to completedVideos
-    courseProgress.completedVideos.push(subSectionId);
-
-    await courseProgress.save();
-
-    return res.status(200).json({
-      message: "Course Progress updated",
-      data: courseProgress,
-    });
   } catch (error) {
-    console.log("Error while updating the courseProgress...", error);
+    console.log(error);
     return res.status(500).json({
-      error: "Error while updating the courseProgress. Internal server error.",
+      error:
+        "Internal server error while updating the courseProgress completedVideos",
     });
   }
 };
