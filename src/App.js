ca13ca49110dac7d2195624d3bb0bec16c36de1b
kash2104 <kparikh1104@gmail.com>
2024-05-18 17:49:52 +0530

    Path added for InstructorDashboard in App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import Catalog from "./pages/Catalog";
 import CourseDetails from "./pages/CourseDetails";
 import ViewCourse from "./pages/ViewCourse";
 import VideoDetails from "./components/core/ViewCourse/VideoDetails";
+import Instructor from "./components/core/Dashboard/InstructorDashboard/Instructor";
 
 function App() {
   const { user } = useSelector((state) => state.profile);
@@ -121,6 +122,8 @@ function App() {
 
           {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
             <>
+              <Route path="dashboard/instructor" element={<Instructor />} />
+
               <Route path="dashboard/add-course" element={<AddCourse />} />
 
               <Route path="dashboard/my-courses" element={<MyCourses />} />
