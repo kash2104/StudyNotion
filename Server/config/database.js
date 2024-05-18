3decaf4f4425d6501e2461dcdbf7149fde743d85
kash2104 <kparikh1104@gmail.com>
2024-05-18 10:05:15 +0530

    database config modified.

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL)
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
-        console.log('DB connected successfully')
+      console.log("DB connected successfully");
     })
     .catch((error) => {
-        console.log('DB connection failed')
-        console.error(error)
-        process.exit(1);
-    })
-}
+      console.log("DB connection failed");
+      console.error(error);
+      process.exit(1);
+    });
+};
